3f77dbabb5ae7caf34e7d045a820685040a4bf43
AlexIsOK <alexep2020@gmail.com>
2020-07-17 07:17:42 -0700

    d

--- src/main/java/dev/alexisok/untitledbot/Main.java
+++ src/main/java/dev/alexisok/untitledbot/Main.java
@@ -35,7 +35,7 @@ import java.util.Scanner;
  */
 public class Main {
 	
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.3.1";
 	public static final String CONFIG_PATH = Paths.get("").toAbsolutePath().toString();
 	public static final String DATA_PATH;
 	public static final String PREFIX;
